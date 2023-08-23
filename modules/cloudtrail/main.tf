857d18d9e319aeaaa7af7f057145e1d3d813d443
John Peter Narzoles <john.narzoles@maya.ph>
2023-08-23 17:50:54 +0800

    fix tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,5 +1,9 @@
 resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
-  enable_logging = false
+  enable_logging = true
+  enable_log_file_validation = true
+  is_multi_region_trail = true
+  kms_key_id = "arn:aws:kms:us-east-1:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
+  cloud_watch_logs_group_arn =  "arn:aws:kms:us-east-1:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
 }
